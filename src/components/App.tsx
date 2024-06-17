3ceefd54eb1de9d4a913c7747c83a08a6aa89818
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-17 19:27:21 +0300

    Home Page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react'
+import GoogleBooksApi from 'functions/GoogleBooksApi'
+import BookGrid from './BookGrid'
+import type { BookSummary } from './BookGrid/BookCard'
+
 function App() {
-  return <div>Test</div>
+  const [books, setBooks] = useState<BookSummary[]>([])
+  useEffect(() => {
+    GoogleBooksApi.searchBooks('javascript').then((books) => {
+      setBooks(books.items)
+    })
+  }, [])
+
+  return (
+    <div className="p-5">
+      <BookGrid books={books} />
+    </div>
+  )
 }
 
 export default App
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,12 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import App from './App'
 
 describe('<App />', () => {
   it('should render the App', () => {
     const { container } = render(<App />)
-
-    expect(screen.getByText(/Test/i)).toBeInTheDocument()
 
     expect(container.firstChild).toBeInTheDocument()
   })
