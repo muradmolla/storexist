5ca177a5062b8f2e920a06dfee366e18f5518b97
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-17 23:23:38 +0300

    Testing: Added mock route to test util

--- src/utils/storeTestUtils.tsx
+++ src/utils/storeTestUtils.tsx
@@ -2,6 +2,7 @@ import { FC, ReactElement, ReactNode } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { AppStore, RootState, setupStore } from 'store'
+import { BrowserRouter } from 'react-router-dom'
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: Partial<RootState>
@@ -16,7 +17,11 @@ const customRender = (
   }: ExtendedRenderOptions = {}
 ) => {
   const AllTheProviders: FC<{ children: ReactNode }> = ({ children }) => {
-    return <Provider store={store}>{children}</Provider>
+    return (
+      <Provider store={store}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </Provider>
+    )
   }
   return {
     store,
