d33ba5bb178d164c4aadebd1bf763101af53c15c
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-17 18:26:53 +0300

    Util: Truncate added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ')
 }
+export function truncate(str: string, maxLength: number): string {
+  return str.length > maxLength ? `${str.slice(0, maxLength)}...` : str
+}
--- src/utils/test.tsx
+++ src/utils/test.tsx
@@ -2,6 +2,7 @@ import Counter from 'components/test/Counter'
 import { render, screen } from 'utils/storeTestUtils'
 import { setupStore } from 'store'
 import { decrement, incrementByAmount } from 'store/slices/count'
+import { truncate } from 'utils'
 
 describe('storeTestUtil', () => {
   it('counter should reach store', () => {
@@ -24,5 +25,14 @@ describe('storeTestUtil', () => {
       render(<Counter />, { store })
       expect(screen.getByTestId('count')).toHaveTextContent('Count: -1')
     })
+  })
+})
+
+describe('Truncate', () => {
+  it('should truncate the text', () => {
+    let truncateText = 'Hello, World!'
+    expect(truncate(truncateText, 5)).toBe('Hello...')
+    truncateText = 'Hello, World! This is a long text.'
+    expect(truncate(truncateText, 1)).toBe('H...')
   })
 })
