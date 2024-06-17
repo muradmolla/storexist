25f24ab42944d342fb272f0767025b46abc6075c
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-17 22:38:28 +0300

    Details Route: Added more details to the page and ui improvement

--- src/routes/Details/index.tsx
+++ src/routes/Details/index.tsx
@@ -1,3 +1,4 @@
+import AddToCart from 'components/AddToCart'
 import GoogleBooksApi from 'functions/GoogleBooksApi'
 import { GoogleBook } from 'functions/GoogleBooksApi/googleTypes'
 import { useLoaderData, type Params } from 'react-router-dom'
@@ -22,6 +23,25 @@ export default function Details() {
 
   return (
     <div className="p-5">
+      <div className="gap-4 md:flex md:justify-between">
+        <div className="my-2 gap-4 md:flex">
+          <img src={book.volumeInfo.imageLinks.small} />
+          <div className="my-4">
+            <p>Author: {book.volumeInfo.authors.join(', ')}</p>
+            <p>Published Date: {book.volumeInfo.publishedDate}</p>
+            <p>Page Count: {book.volumeInfo.pageCount}</p>
+            <p>Categories: {book.volumeInfo.categories.join(', ')}</p>
+            <p>Language: {book.volumeInfo.language}</p>
+          </div>
+        </div>
+        <div className="my-4">
+          <p>
+            Price: {book.saleInfo.retailPrice?.amount}{' '}
+            {book.saleInfo.retailPrice?.currencyCode}
+          </p>
+          <AddToCart id={book.id} />
+        </div>
+      </div>
       <h1 className="text-2xl font-bold">{book.volumeInfo.title}</h1>
       <p className="text-gray-500">{book.volumeInfo.authors.join(', ')}</p>
       <p>{book.volumeInfo.description}</p>
