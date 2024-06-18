992cb95993183d1abc4e112d1a8e5129746b841b
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-18 08:50:30 +0300

    Layout: Added home link to logo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from 'react'
 import logo from 'assets/logo.svg'
 import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="container mx-auto">
       <div>
-        <img src={logo} className="mx-auto w-80" alt="logo" />
+        <Link to="/">
+          <img src={logo} className="mx-auto w-80" alt="logo" />
+        </Link>
       </div>
       <div className="bg-slate-900 p-2 text-white">
         <SearchBar />
