8238d353341d4de976b4c0186efc3f072c288167
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-18 21:07:11 +0300

    FinalCheckout: Added validation and mock function

--- src/routes/Checkout/FinalCheckout/index.tsx
+++ src/routes/Checkout/FinalCheckout/index.tsx
@@ -1,11 +1,35 @@
 import { ActionFunction } from 'react-router-dom'
+
+interface CheckoutFormData {
+  books: string[]
+  name: string
+  address: string
+  city: string
+}
+
+async function mockApiRequest(data: CheckoutFormData) {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  console.log(data)
+}
+
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
   const books = formData.get('books')
   const name = formData.get('fullname')
   const address = formData.get('address')
   const city = formData.get('city')
-  console.log(books, name, address, city)
+  if (!books || !name || !address || !city) {
+    return { status: 400, error: 'Missing required fields' }
+  }
+  if (
+    !Array.isArray(books) ||
+    typeof name !== 'string' ||
+    typeof address !== 'string' ||
+    typeof city !== 'string'
+  ) {
+    return { status: 400, error: 'Invalid data' }
+  }
+  await mockApiRequest({ books, name, address, city })
 
   return { formData }
 }
