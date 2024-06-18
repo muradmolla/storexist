b644b8437d7e63f8da0231a7bf66f32fca167aa1
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-18 13:57:31 +0300

    Component: Add to cart component dispatches addBook reducer

--- src/components/AddToCart/index.tsx
+++ src/components/AddToCart/index.tsx
@@ -1,4 +1,14 @@
+import { useAppDispatch } from 'store/hooks'
+import { addBook } from 'store/slices/cart'
+
 export default function AddToCart({ id }: { id: string }) {
-  console.log('AddToCart', id)
-  return <button className="rounded bg-green-400 px-2">Add to Cart</button>
+  const dispatch = useAppDispatch()
+  return (
+    <button
+      onClick={() => dispatch(addBook({ id }))}
+      className="rounded bg-green-400 px-2"
+    >
+      Add to Cart
+    </button>
+  )
 }
