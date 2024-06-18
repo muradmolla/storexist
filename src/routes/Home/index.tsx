3b6443c8c7a39a3f557a64d399c710b4be5588e9
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-18 21:56:45 +0300

    Home: Pagination

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -3,18 +3,24 @@ import GoogleBooksApi from 'functions/GoogleBooksApi'
 import BookGrid from 'components/BookGrid'
 
 import type { BookSummary } from 'components/BookGrid/BookCard'
+import Paginate from 'components/Paginate'
 
 export default function Home() {
   const [books, setBooks] = useState<BookSummary[]>([])
+  const [maxPage, setMaxPage] = useState(1)
+  const [page, setPage] = useState(1)
   useEffect(() => {
-    GoogleBooksApi.searchBooks('javascript').then((books) => {
+    GoogleBooksApi.searchBooks('javascript', page).then((books) => {
       setBooks(books.items)
+      console.log(books)
+      setMaxPage(Math.ceil(books.totalItems / 10))
     })
-  }, [])
+  }, [page])
 
   return (
     <div className="p-5">
       <BookGrid books={books} />
+      <Paginate maxPage={maxPage} currentPage={page} pageCall={setPage} />
     </div>
   )
 }
