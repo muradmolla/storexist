032b7abe9e182e4854c4501fffa3217bc017b28f
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-18 21:55:51 +0300

    GoogleBooksApi: Added pagination

--- src/functions/GoogleBooksApi/index.ts
+++ src/functions/GoogleBooksApi/index.ts
@@ -2,10 +2,17 @@ import googleRequest from './googleRequest'
 import type { GoogleBook, GoogleBooksResponse } from './googleTypes'
 
 const GoogleBooksApi = {
-  searchBooks: async (query: string): Promise<GoogleBooksResponse> => {
+  searchBooks: async (
+    query: string,
+    page: number | null = null
+  ): Promise<GoogleBooksResponse> => {
+    const urlParams = new URLSearchParams({ q: query })
+    if (page) {
+      urlParams.append('startIndex', ((page - 1) * 10).toString())
+    }
     const response = googleRequest.get<GoogleBooksResponse>(
       'volumes',
-      new URLSearchParams({ q: query })
+      urlParams
     )
     return response
   },
