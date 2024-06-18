3657904cefe5c20cf9406b50b9188f13ec31ccf0
Murad Mollaoğlu <muradmollaoglu@gmail.com>
2024-06-18 10:17:39 +0300

    Layout: Menu responsiveness

---
+++ src/components/Layout/Menu/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import SearchBar from '../SearchBar'
+import logo from 'assets/logo.svg'
+import { classNames } from 'utils'
+import { IoIosMenu, IoIosClose } from 'react-icons/io'
+
+export default function Menu() {
+  const [open, setOpen] = useState(false)
+  const openClasses = 'h-screen md:h-auto'
+  const closedClasses = ''
+  const defaultClasses = 'bg-slate-300'
+  return (
+    <div
+      className={classNames(defaultClasses, open ? openClasses : closedClasses)}
+    >
+      <div className="container mx-auto md:flex">
+        <div className="flex items-center justify-between">
+          <Link to="/">
+            <img src={logo} className="mx-auto w-60" alt="logo" />
+          </Link>
+          <button className="mr-4 md:hidden" onClick={() => setOpen(!open)}>
+            {open ? <IoIosClose size="2em" /> : <IoIosMenu size="1.5em" />}
+          </button>
+        </div>
+        <div
+          className={classNames(
+            'md:block md:flex md:w-full md:justify-between items-center',
+            open ? '' : 'hidden'
+          )}
+        >
+          <div className="md:ml-32">
+            <SearchBar />
+          </div>
+          <button>Cart</button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,20 +1,15 @@
 import { ReactNode } from 'react'
-import logo from 'assets/logo.svg'
-import SearchBar from './SearchBar'
-import { Link } from 'react-router-dom'
+import Menu from './Menu'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="container mx-auto">
-      <div>
-        <Link to="/">
-          <img src={logo} className="mx-auto w-80" alt="logo" />
-        </Link>
+    <div>
+      <div className="sticky top-0 w-full">
+        <div className="w-full">
+          <Menu />
+        </div>
       </div>
-      <div className="bg-slate-900 p-2 text-white">
-        <SearchBar />
-      </div>
-      <div className="p-2 py-4">{children}</div>
+      <div className="container mx-auto p-2 py-4">{children}</div>
     </div>
   )
 }
